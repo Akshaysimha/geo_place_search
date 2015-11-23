5011ebff4a69999f3f3589821ad6fad82c70d958
asimha <asimha@qwinixtech.com>
2015-11-23 12:44:54 +0530

    refactor

--- lib/geo_place_search.rb
+++ lib/geo_place_search.rb
@@ -5,6 +5,8 @@ require 'pry'
 
 class GeoPlaceSearch
   def self.search_places(keyword, api_key)
+
+    parse_json('https://maps.googleapis.com/maps/api/place/textsearch/json?query=#{keyword}&key=#{api_key}')
     uri = URI.parse("https://maps.googleapis.com/maps/api/place/textsearch/json?query=#{keyword}&key=#{api_key}")
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
@@ -51,5 +53,15 @@ class GeoPlaceSearch
       search_results << p
     end
     search_results
+  end
+
+  private
+
+  def parse_json(api)
+    uri = URI.parse(api)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    request = Net::HTTP::Get.new(uri.request_uri)
+    @response = http.request(request)  
   end
 end
